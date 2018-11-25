97af1ed7c7ee8167f09bd5eef1548c73807ce642
f1rat <firat@siktirgit.xyz>
2018-11-26 02:54:20 +0300

    v2

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,43 +1,253 @@
-// Initialize app
-var myApp = new Framework7();
+"use strict";
 
+/*===============================================*/
+/* APP INIT		 						         */
+/*===============================================*/
+(function() {
+var myApp = new Framework7({
 
-// If we need to use custom DOM library, let's save it to $$ variable:
+	routes: [
+    {
+      name: 'main',
+      path: '',
+      url: './main.html',
+      on: {
+        pageAfterIn: function (e, page) {
+	},
+        pageInit: function (e, page) {
+		  getSlides();
+          getLatestNews();
+		  getLatestListings();
+		          },
+      }
+    },
+    {
+      path: '/blog/',
+      url: './blog.html',
+      on: {
+      pageInit: function (e, page) {
+            getFullNews();
+      },
+      }
+    },
+        {
+      path: '/add-product/',
+      url: './add-product.html',
+      on: {
+      pageInit: function (e, page) {
+      getCatListingForDropDown();
+      },
+      }
+    },
+    {
+      path: '/blank/',
+      url: './_blank.html',
+      on: {
+      pageInit: function (e, page) {
+
+//do whatever
+
+      },
+      }
+    },
+     {
+      path: '/machinecatlisting/',
+      url: './machine-cat-listing.html',
+      on: {
+      pageInit: function (e, page) {
+            getAllMachines();
+      },
+      }
+    },
+    {
+      path: '/autocat/',
+      url: './auto-categories.html',
+      on: {
+      pageInit: function (e, page) {
+            getCatListing();
+      },
+      }
+    },
+      {
+      path: '/booking/',
+      url: './booking-form.html',
+    },
+    {
+      path: '/faqs/',
+      url: './faqs.html'
+    },
+    {
+      path: '/how-it-works/',
+      url: './how-it-works.html'
+    },
+    {
+      path: '/junk-cars-search/',
+      url: './junk-cars-search.html'
+    },
+    {
+      path: '/login/',
+      url: './login.html'
+    },
+    {
+      path: '/product-category/',
+      url: './product-category.html'
+    },
+    {
+      path: '/product-single/',
+      url: './product-single.html',
+      on: {
+      pageInit: function (e, page) {
+            displayProduct();
+      },
+      }
+    },
+    {
+      path: '/register/',
+      url: './register.html'
+    },
+    {
+      path: '/remember-password/',
+      url: './remember-password.html'
+    },
+    {
+      path: '/sitemap/',
+      url: './sitemap.html'
+    },
+    {
+      path: '/used-car-parts/',
+      url: './used-car-parts.html'
+    },
+    {
+      path: '/users/:userId/',
+      url: './user.html',
+    },
+    {
+      path: '/blog-read/',
+      url: './blog-read.html',
+      on: {
+      pageInit: function (e, page) {
+            displayFullNews();
+      },
+      }
+    },
+    ],
+  
+  
+	root: '#app',
+	theme: 'ios',
+	swipePanel: 'left',
+	touch: {
+    tapHold: true,
+	},
+	view: {
+    xhrCache: false,
+    pushState: true,
+    uniqueHistory: true,
+ 	},
+ 	popup: {
+    closeByBackdropClick: true,
+	},
+	lazy: {
+    threshold: 50,
+    },
+    statusbar: {
+    overlay: true,
+    scrollTopOnClick: true,
+    },
+		
+});
+
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    loop: true,
+    autoplay: {
+    delay: 5000,
+    }
+});
+
+/*===============================================*/
+/* EXPORT SELECTORS ENGINE		 			     */
+/*===============================================*/
 var $$ = Dom7;
 
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
-    dynamicNavbar: true
+/*===============================================*/
+/* ADD VIEW		 			     				 */
+/*===============================================*/
+var mainView = myApp.view.create('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true,
+    preloadPreviousPage: false,
+    iosSwipeBack: false
 });
 
-// Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+
+/*=========================================================*/
+/* SHOW/HIDE PRELOADER FOR REMOTE AJAX LOADED PAGES		   */
+/*=========================================================*/
+
+$$(document).on('ajaxStart', function (e) {
+    myApp.showIndicator();
+});
+$$(document).on('ajaxComplete', function () {
+    myApp.hideIndicator();
 });
 
 
-// Now we need to run the code that will be executed only for About page.
 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+/*==================================================================*/
+/* PAGE INIT : HERE, YOU CAN WRITE YOUR CUSTOM JAVASCRIPT/JQUERY    */
+/*==================================================================*/
 
-})
+$$(document).on('pageInit', function (e) 
+{
 
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
 
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
-    }
-})
 
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+
+
+	
+	/* SLIDE SLICK 
+	================================*/
+	var slickOpts = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		arrows: false,
+        dots: true,
+		centerMode: true,
+		centerPadding: '15px',
+		adaptiveHeight: true,
+    };
+    $('#walkthrough-items').slick(slickOpts);
+	
+	var calendarDefault = myApp.calendar({
+		input: '#calendar-default',
+	});
+	
+	
+	var pickerDescribe = myApp.picker({
+		input: '#picker-time',
+		cols: [
+			{
+				textAlign: 'left',
+				values: (function () {
+					var arr = [];
+					for (var i = 0; i <= 23; i++) { arr.push(i); }
+					return arr;
+				})(),
+			},
+			{
+				values: ('PM AM').split(' ')
+			},
+		]
+				
+	});
+}
+);
+
+
+ 
+//And now we initialize app
+myApp.init();
+
+	})(); 
